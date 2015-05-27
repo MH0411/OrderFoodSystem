4e85230f4078c9658a5c783f3bf387c076b5b90c
Mike <z.mike0411@gmail.com>
2015-05-28 01:13:38 +0800

    Changed java version to 1.8
    28/5/15 1:13Am

---
+++ src/GUI/LoginGUI.java
@@ -0,0 +1,48 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JSplitPane;
+import java.awt.Component;
+import javax.swing.Box;
+
+public class LoginGUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginGUI frame = new LoginGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 696, 479);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		Component verticalStrut = Box.createVerticalStrut(442);
+		contentPane.add(verticalStrut, BorderLayout.NORTH);
+	}
+
+}
